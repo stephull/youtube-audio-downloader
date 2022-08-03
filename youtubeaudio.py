ca1b3705bd6d2c312e5b4980d7fdde0ab4b455a4
Stephen Hullender <shullender0907@gmail.com>
2022-08-03 17:14:26 -0400

    add deletion from queue, more regex, fontstyles, and gitignore

--- youtubeaudio.py
+++ youtubeaudio.py
@@ -1,14 +1,22 @@
 import tkinter as tk 
-from tkinter import filedialog
-import subprocess, os, re
+from tkinter import filedialog, font
+import customytaudioconfigs as config
+import subprocess, re, os, sys
 
 # stack for queueing multiple items
 URLLIST = []
 
 # dimensions
-windowHeight = 420
-windowWidth = 625
+windowHeight = 480
+windowWidth = 630
 windowScaling = 1.5
+textData = ("Helvetica", 10)
+textDataBold = ("Helvetica", 10, font.BOLD)
+YOUTUBE_URL_LENGTH = 42
+
+# regex
+clearChannel = '(&ab_)\w+=[\w\d\_\-]+'
+clearIndices = '^[()]\d+[()]\s'
 
 # essential Tkinter window items
 root = tk.Tk()
@@ -26,10 +34,14 @@ donelabel = tk.Label(root, text="Complete!", fg='black', bg='green2')
 exceptionlabel = tk.Label(root, text="Error, link could not be completed!", bg='red', fg='white')
 
 # input Youtube URL 
-inputlabel = tk.Label(root, text="::: YouTube Audio Downloader :::\n\nInsert YouTube link...")
+inputlabel = tk.Label(
+    root, 
+    text="::: YouTube Audio Downloader :::\nInsert YouTube link...",
+    font=textDataBold    
+)
 inputlabel.grid(row=0, column=0, padx=2, pady=5)
 inputtxt = tk.Text(root, height=2, width=40)
-inputtxt.configure(font=("Helvetica", 10))
+inputtxt.configure(font=textData)
 inputtxt.grid(row=1, column=0, padx=5, pady=10)
 
 #
@@ -43,7 +55,7 @@ def saveto():
     savetofolder = filedialog.askdirectory(initialdir=os.path.normpath("/mnt/c/"), title="Save to Folder")
     cwdlabel.configure(text=f"Currently saved in {savetofolder}", bg='yellow', fg='black')
 
-outputdirlabel = tk.Label(root, text='Where do you want to save the audio?')
+outputdirlabel = tk.Label(root, text='Where do you want to save the audio?', font=textDataBold)
 outputdirlabel.grid(row=2, column=0, padx=2, pady=5)
 outputbtn = tk.Button(root, text='Save to...', command=saveto, bg='green', fg='white')
 outputbtn.grid(row=2, column=1, padx=2, pady=5)
@@ -58,44 +70,69 @@ def convert(link):
         savetofolder = savetofolder.replace("C:/", "/mnt/c/")
     savetofolder = f"{savetofolder}/"
     
-    channelURI = "&ab_"
-    youtubeURL = re.sub(f'({channelURI})\w+=[\w\d\$\_\-]+', '', link)
+    youtubeURL = re.sub(clearChannel, '', link)
     
-    bashCmd = f"bash ./convert.sh {savetofolder} {youtubeURL}"
-    process = subprocess.Popen(bashCmd.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        ["bash", "./convert.sh", savetofolder, youtubeURL],
+        stdout=subprocess.PIPE
+    )
     
     output, err = process.communicate()
-    if (err != None):
-        print(f"::: ERROR :::\n{err}")
-        exceptionlabel.grid(row=9, column=0, padx=2, pady=5)
-    else:
-        print(f"::: OUTPUT :::\n{output}")
-        donelabel.grid(row=9, column=0, padx=2, pady=5)
-    inputtxt.delete("1.0", "end-1c")
+    print(f"::: ERROR :::\n{err}" if err != None else f"::: OUTPUT :::\n{output}")
+    new_label = exceptionlabel if err != None else donelabel
+    new_label.grid(row=9, column=0, padx=2, pady=5)
+    inputtxt.delete(1.0, "end-1c")
     
 def download():
     inp = inputtxt.get(1.0, "end-1c")
-    if len(inp) > 0:
-        convert(inp)
+    if len(inp) >= YOUTUBE_URL_LENGTH:
+        convert(inp) 
 downloadNowButton = tk.Button(root, command=download, text='Download', bg='blue', fg='white')
 downloadNowButton.grid(row=1, column=1, padx=2, pady=5)
 
 # button and command to add to queue
 def addtoqueue():
     inp = inputtxt.get("1.0", "end-1c")
+    if len(inp) < YOUTUBE_URL_LENGTH:
+        return 
     tmp_index = len(URLLIST)
     URLLIST.append(inp)
-    listcanvas.insert(tmp_index, URLLIST[tmp_index])
-    inputtxt.delete("1.0", "end-1c")
+    listcanvas.insert(tmp_index, f"({tmp_index}) {URLLIST[tmp_index]}")
+    inputtxt.delete(1.0, "end-1c")
 addToQueueButton = tk.Button(root, command=addtoqueue, text='Add To Queue', bg='purple', fg='white')
 addToQueueButton.grid(row=1, column=2, padx=2, pady=5)
+
+# delete selection from queue (needs index number)
+def deletefromqueue():
+    try:
+        index = int(delFromSelection.get(1.0, "end-1c"))
+        delFromSelection.delete(1.0, "end-1c")
+    except ValueError:
+        return 
+    listcanvas.delete(index)
+    URLLIST.pop(index)
+    for i in range(index, len(URLLIST)):
+        list_value = listcanvas.get(i)
+        list_value = re.sub(clearIndices, '', list_value)
+        listcanvas.delete(i)
+        listcanvas.insert(i, f"({i}) {list_value}")
+delFromQueueButton = tk.Button(root, command=deletefromqueue, text='Delete Selection', bg='red', fg='white')
+delFromQueueButton.grid(row=7, column=1, padx=2, pady=5)
+
+delFrame = tk.Frame(root)
+delFromLabel = tk.Label(delFrame, text='Type index number:', font=('Helvetica', 7, font.ITALIC))
+delFromLabel.pack()
+delFromSelection = tk.Text(delFrame, width=3, height=1)
+delFromSelection.pack()
+delFrame.grid(row=7, column=2, padx=2, pady=5)
 
 # button and commmand for downloading all links
 def downloadAll():
     for i in range(len(URLLIST)):
         print(URLLIST[i])
-        tmp_download = listcanvas.get(0)
-        convert(tmp_download)
+        downloading = listcanvas.get(0)
+        downloading = re.sub(clearIndices, '', downloading)
+        convert(downloading)
         listcanvas.delete(0)
     URLLIST.clear()
 downloadAllButton = tk.Button(root, command=downloadAll, text='Download All', bg='blue', fg='white')
@@ -107,14 +144,12 @@ downloadAllButton.grid(row=6, column=1, padx=2, pady=5)
 
 # components of tkinter window
 msglabel = tk.Label(root, text="Window freezes when downloading, wait until you see 'Complete!'")
-msglabel.grid(row=7, column=0, padx=2, pady=5)
+msglabel.grid(row=8, column=0, padx=2, pady=5)
 
 # show list from URLLIST
-listcanvaslabel = tk.Label(root, text='Queue')
+listcanvaslabel = tk.Label(root, text='Queue', font=textDataBold)
 listcanvaslabel.grid(row=5, column=0, padx=5, pady=5)
-listcanvas = tk.Listbox(root, height=5, width=50, bg='grey', activestyle='dotbox', fg='white')
-for i in range(len(URLLIST)):
-    listcanvas.insert(i, URLLIST[i])
-listcanvas.grid(row=6, column=0, padx=2, pady=5)
+listcanvas = tk.Listbox(root, height=8, width=50, bg='grey', activestyle='dotbox', fg='white')
+listcanvas.grid(row=6, column=0, rowspan=2, padx=2, pady=5)
 
 root.mainloop()
